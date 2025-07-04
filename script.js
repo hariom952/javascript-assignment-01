195210dc8aeab86cc168a1b634586fe47ac2689c
hariom952 <siddharthrai402@gmail.com>
2025-07-04 10:21:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var a=2
+var b=5
+console.log(a+b)
+
+
+
+let x=5
+let y=3
+console.log(x>y?"True":"False")
+
+
+
+let e=12
+let f=20
+if((e%10==0) && (f%10==0))
+    console.log(true);
+else if((e%10!=0) && (f%10!=0))
+    console.log(false);
+else if((e%10==0) || (f%10==0))
+    console.log(true);
+
+
+
+var v=4567
+let res=v/1000
+console.log(Math.floor(res))
+
+
+let N = 4567 
+let l= N % 10 
+console.log(l)
+
+
+
+let num11= 2; 
+let num12= 9; 
+let rem=num12%num11
+console.log(rem)
+
+
+let u1=2
+let u2=5
+console.log(u1*u2)
+
+
+let c1=50
+let c2=20
+let c3=100
+let sum=c1+c2+c3
+let avg=sum/3
+console.log("Total ="+ sum)
+console.log("Average ="+ avg)
